8237875c717955451990cedd79847f3df6e05964
Cordial Black <40433796+cordialblack@users.noreply.github.com>
2018-09-14 11:03:38 -0500

    Initial commit

---
+++ make_home.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+files=('.bash_profile' '.bashrc' '.bash_alisases' )
+
+for i in ${files[@]}; do
+
+	if [ -f $HOME/$i.old ]; then
+		rm -f $HOME/$i.old
+	fi
+
+	if [ -f $HOME/$i ]; then
+		mv $HOME/$i $HOME/$i.old
+	fi
+done
+
+ln -s $PWD/bash_profile $HOME/.bash_profile
+ln -s $PWD/bashrc $HOME/.bashrc
